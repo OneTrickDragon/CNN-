dc10d45394b2ed108ac941eb536014a5308e43b2
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-05-27 23:49:54 +0530

    dataset

--- cnn.py
+++ cnn.py
@@ -1,3 +1,18 @@
 import torch
 import torchvision
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
 
+data_dir = "input/seg_train/seg_train/"
+test_data_dir = "input/seg_test/seg_test"
+
+dataset = ImageFolder(data_dir,transform = transforms.Compose([
+    transforms.Resize((150,150)),transforms.ToTensor()
+]))
+
+test_dataset = ImageFolder(test_data_dir,transforms.Compose([
+    transforms.Resize((150,150)),transforms.ToTensor()
+]))
+
+img, label = dataset[0]
+print(img.shape,label)
