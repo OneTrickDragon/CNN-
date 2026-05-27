fc674ed1591400f9687bfb068ad9781f1096781a
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-05-27 23:55:44 +0530

    a

--- cnn.py
+++ cnn.py
@@ -2,6 +2,10 @@ import torch
 import torchvision
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
 
 data_dir = "input/seg_train/seg_train/"
 test_data_dir = "input/seg_test/seg_test"
@@ -16,3 +20,18 @@ test_dataset = ImageFolder(test_data_dir,transforms.Compose([
 
 img, label = dataset[0]
 print(img.shape,label)
+
+def display_img(img,label):
+    print(f"Label : {dataset.classes[label]}")
+    plt.imshow(img.permute(1,2,0))
+
+batch_size = 128
+val_size = 2000
+train_size = len(dataset) - val_size 
+
+train_data,val_data = random_split(dataset,[train_size,val_size])
+print(f"Length of Train Data : {len(train_data)}")
+print(f"Length of Validation Data : {len(val_data)}")
+
+train_dl = DataLoader(train_data, batch_size, shuffle = True, num_workers = 4, pin_memory = True)
+val_dl = DataLoader(val_data, batch_size*2, num_workers = 4, pin_memory = True)
