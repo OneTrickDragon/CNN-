203bef6663b0329920f3bfbbb9fe2e9af6af3242
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-06-04 22:18:54 +0530

    testing

--- test.py
+++ test.py
@@ -28,3 +28,68 @@ def predict(dataset_pred, img_name, transformer):
 
 
 if __name__ == "__main__":
+
+    p = configargparse.ArgumentParser()
+    p.add_argument('--dataset_pred', type=str, default='seg_pred', help='Dataset predictions path.')
+    p.add_argument('--log_dir', type=str, default='image_classification_ConvNet', help='Name of the folder to load the model.')
+    p.add_argument('--checkpoint', type=str, default='checkpoint_18_best.pth',help='Checkpoint path')
+    p.add_argument('--device', type=str, default='gpu', help='Choose the device: "gpu" or "cpu"')
+    opt = p.parse_args()
+
+    assert os.path.isdir(opt.log_dir), 'The folder log_dir does not exists'
+
+    if opt.device == 'gpu' and torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        print('Device assigned: GPU (' + torch.cuda.get_device_name(device) + ')\n')
+    else:
+        device = torch.device("cpu")
+        if not torch.cuda.is_available() and opt.device == 'gpu':
+            print('GPU not available, device assigned: CPU\n')
+        else:
+            print('Device assigned: CPU\n')
+
+    classes = os.listdir("seg_test")
+    images = os.listdir(opt.dataset_pred)
+    num_images = len(images)
+    
+    transformer = transforms.Compose([
+        transforms.Resize((150,150)),
+        transforms.ToTensor(),  # 0-255 to 0-1, numpy to tensors
+        # formula: output[channel] = (input[channel] - mean[channel]) / std[channel]
+        transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1]
+                            [0.5,0.5,0.5])
+    ])
+
+    model = ConvNet(num_classes=len(classes)).to(device)
+    state_dict = torch.load(opt.log_dir + "/checkpoints/" + opt.checkpoint, map_location=device)
+    model.load_state_dict(state_dict)
+    model.eval()
+    
+    predictions_dict={
+        # Name of the image
+        "image": [],
+        
+        # Class predicted for the image
+        "class": [],
+        
+        # Probability that the image is the predicted class
+        "probability": [],
+        
+        # Probabilities predicted for each class
+        "probabilities": []
+    }
+
+    for i, img_name in enumerate(images):
+        
+        print('\rPredicting images... [' + str(i+1) + '/' + str(num_images) + ']', end='')
+        
+        predicted_class, probability, predictions = predict(opt.dataset_pred, img_name, transformer)
+        
+        predictions_dict["image"].append(img_name)
+        predictions_dict["class"].append(predicted_class)
+        predictions_dict["probability"].append(str(probability))
+        predictions_dict["probabilities"].append(predictions.tolist())
+        
+        
+    with open(opt.log_dir + '/predictions.json', "w") as fp:
+        json.dump(predictions_dict, fp, indent=4)
